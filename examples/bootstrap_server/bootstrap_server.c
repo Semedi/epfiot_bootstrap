3248f7beceb3339ea4d557d3ef60fc2078bbf32c
Sergio Semedi <sergio.semedi@keepler.io>
2020-07-07 22:17:09 +0200

    curl.h added

--- examples/bootstrap_server/bootstrap_server.c
+++ examples/bootstrap_server/bootstrap_server.c
@@ -39,6 +39,8 @@
 #include "connection.h"
 #include "bootstrap_info.h"
 
+#include <curl/curl.h>
+
 #define CMD_STATUS_NEW  0
 #define CMD_STATUS_SENT 1
 #define CMD_STATUS_OK   2
@@ -500,6 +502,52 @@ int main(int argc, char *argv[])
     FILE * fd;
     int esock;
 
+
+
+
+
+
+
+
+
+    //=============================0
+    // CURL TEST
+    //=============================0
+
+    CURL *curl;
+    CURLcode res;
+
+    /* In windows, this will init the winsock stuff */ 
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    /* get a curl handle */ 
+    curl = curl_easy_init();
+    if(curl) {
+      /* First set the URL that is about to receive our POST. This URL can
+         just as well be a https:// URL if that is what should receive the
+         data. */ 
+      curl_easy_setopt(curl, CURLOPT_URL, "http://postit.example.com/moo.cgi");
+      /* Now specify the POST data */ 
+      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel&project=curl");
+
+      /* Perform the request, res will get the return code */ 
+      res = curl_easy_perform(curl);
+      /* Check for errors */ 
+      if(res != CURLE_OK)
+        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+
+      /* always cleanup */ 
+      curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+
+
+
+
+    //=============================0
+    // END
+    //=============================0
 
     command_desc_t commands[] =
     {
