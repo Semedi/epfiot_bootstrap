7b0e874817d7a9581e6a84c8f9f56bce592bbc85
Sergio Semedi <sergio.semedi@keepler.io>
2020-07-08 17:05:06 +0200

    added curl communication

--- examples/bootstrap_server/bootstrap_server.c
+++ examples/bootstrap_server/bootstrap_server.c
@@ -312,6 +312,10 @@ static int prv_bootstrap_callback(void * sessionH,
     internal_data_t * dataP = (internal_data_t *)userData;
     endpoint_t * endP;
 
+    CURL *curl;
+    CURLcode res;
+    curl_global_init(CURL_GLOBAL_ALL);
+
     switch (status)
     {
     case COAP_NO_ERROR:
@@ -342,6 +346,25 @@ static int prv_bootstrap_callback(void * sessionH,
                   endInfoP = endInfoP->next;
               }
           }
+
+          curl = curl_easy_init();
+
+          if(curl) {
+
+            curl_easy_setopt(curl, CURLOPT_URL, "http://localhost:8080/bootstrap");
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel");
+
+            res = curl_easy_perform(curl);
+
+            if(res != CURLE_OK)
+              fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                      curl_easy_strerror(res));
+
+            curl_easy_cleanup(curl);
+          }
+
+          curl_global_cleanup();
+
         }
         // Nothing found, discard the request
         if (endInfoP == NULL)
@@ -504,43 +527,6 @@ int main(int argc, char *argv[])
 
 
 
-
-
-
-
-
-
-    //=============================0
-    // CURL TEST
-    //=============================0
-
-    CURL *curl;
-    CURLcode res;
-
-    /* In windows, this will init the winsock stuff */ 
-    curl_global_init(CURL_GLOBAL_ALL);
-
-    /* get a curl handle */ 
-    curl = curl_easy_init();
-    if(curl) {
-      /* First set the URL that is about to receive our POST. This URL can
-         just as well be a https:// URL if that is what should receive the
-         data. */ 
-      curl_easy_setopt(curl, CURLOPT_URL, "http://postit.example.com/moo.cgi");
-      /* Now specify the POST data */ 
-      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel&project=curl");
-
-      /* Perform the request, res will get the return code */ 
-      res = curl_easy_perform(curl);
-      /* Check for errors */ 
-      if(res != CURLE_OK)
-        fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                curl_easy_strerror(res));
-
-      /* always cleanup */ 
-      curl_easy_cleanup(curl);
-    }
-    curl_global_cleanup();
 
 
 
